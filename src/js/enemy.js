b0f7833915b2d39d0cefae6448a5e2ce45ffd367
Derrak Richard <derrak@gmail.com>
2022-07-06 13:13:04 -0700

    add enemy service; enemy moved R to L once

--- src/js/enemy.js
+++ src/js/enemy.js
@@ -1,10 +1,20 @@
-
-  //Enemy object && function
-  
- export default class Enemy {
-
+export default class Enemy {
+  constructor(gameWidth,gameHeight){
+    this.gameWidth = gameWidth;
+    this.gameHeight = gameHeight;
+    this.width = 170;
+    this.height = 100;
+    this.image = document.getElementById("enemyImage");
+    this.x = this.gameWidth;
+    this.y = this.gameHeight - this.height-10;
+    this.frameX = 0;
   }
-  // Animating, adding && removing enemies
-  function handleEnemies(){
-
+  draw(context){
+    context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height);
   }
+  update(){
+    this.x--;
+  }
+
+}
+
